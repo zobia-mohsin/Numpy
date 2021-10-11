89cafaa9be6ad6eaba036b7ed8ac2f65e999254c
Zobia Mohsin <zobia_mohsin1@baylor.edu>
2021-10-11 15:46:04 -0500

    Creates flattened, ravel, etc to move pandas

--- MyArrays1.py
+++ MyArrays1.py
@@ -41,5 +41,59 @@ arr07 = np.arange(5, 10)  # lower and upper bounds of arrange
 
 arr08 = np.arange(10, 1, -2)
 
+arr09 = np.linspace(0.0, 1.0, num=5)
 
-print()  # start using debugger, shape tells you 2 rows 3 columns, size tells how many total elements
+arr10 = np.arange(1, 21).reshape(4, 5)
+# to reshape something make wsure it is the right sice
+
+num01 = np.arange(1, 6)
+
+num02 = num01 * 2
+#num02 = num01 [2, 2, 2, 2, 2]
+# unequal arrays broadcasting value
+
+num03 = num01 ** 3
+
+num01 += 10
+# this is changing the original arrays,
+# but broadcasting does not change the orginal arrays
+
+num04 = num01 * num02
+# this will only work if those two arrays have same size and shape
+
+num05 = num01 > 13
+
+num06 = num03 > num01  # returns true and false
+# building blocks of pandas
+
+grades = np.array([[87, 96, 70], [100, 87, 90], [94, 77, 90], [100, 81, 82]])
+
+print(grades.sum())
+print(grades.mean())
+print(grades.std())
+print(grades.var())
+
+grades_by_exam = grades.mean(axis=0)
+
+grades_by_student = grades.mean(axis=1)
+
+num07 = np.array([1, 4, 9, 16, 25, 36])
+num08 = np.sqrt(num07)
+
+num09 = np.array([10, 20, 30, 40, 50, 60])
+num10 = np.add(num07, num09)
+
+num11 = np.multiply(num09, 5)
+
+num12 = num09.reshape(2, 3)  # three rows of two columns
+num13 = np.array([2, 4, 6])
+
+# num12 and num13 have different shapes, would that work?
+# works because number of columns match (to multiply)
+num14 = np.multiply(num12, num13)
+
+
+print()
+
+
+# print()  # start using debugger, shape tells you 2 rows 3 columns, size tells how many total elements
---
+++ MyArrays2.py
@@ -0,0 +1,86 @@
+import numpy as np
+
+
+# Here we have an array of 4 students of 4 students grades on 3 exams
+# row = each student(student0,student1, studetn2, studetn3)
+# COLS = eact test (test0, test1,test2)
+
+grades = np.array([[87, 96, 70], [100, 87, 90],
+                   [94, 77, 90], [100, 81, 82]])
+
+# Indexing and Slicing, look at particular rows and slice accordingly
+
+student1 = grades[1]
+
+student0_test1 = grades[0, 1]
+
+# sequential rows use columns, upper bound not included
+# colons mean consecutive rows : but commas to separate them
+students0_1 = grades[0:2]
+
+# outer brackets row and column, inner brackets 1 and 3 rows
+students1and3 = grades[[1, 3]]
+# want particular column as , and specify column you want
+students1and3_test2 = grades[[1, 3], 2]
+# first element is rows so colons mean all rows, column 0
+all_students_test0 = grades[:, 0]
+
+# upper bound 3 is not included gives test 1 and 2
+all_students_test1_2 = grades[:, 1:3]
+
+all_students_test0and2 = grades[:, [0, 2]]
+
+
+# EXERCISE
+grades = np.random.randint(60, 101, 12).reshape(3, 4)  # 101 to INCLUDE 100
+
+grades.mean()
+
+# by col
+grades.mean(axis=0)
+
+# by row
+grades.mean(axis=1)
+
+numbers = np.arange(1, 6)
+
+numbers_view = numbers.view()
+
+numbers[1] *= 10
+
+# change view to get back array, CHANGE IN ONE EFFECTS OTHER SHALLOW COPY .view()
+numbers_view[1] /= 10
+
+# slice effects the original array too
+numbers_slice_view = numbers[0:3]
+
+# when you are working with indexes, usually start with index 0
+numbers[1] *= 20
+
+# deep copy does not effect the orginal copy, .copy() VIEW AND SLICE DOES THE SAME THING
+numbers_copy = numbers.copy()
+
+numbers[1] *= 10
+
+grades = np.array([[87, 96, 70], [100, 87, 90]])
+
+# creates a shallow copy
+grades_reshaped = grades.reshape(1, 6)
+
+# modifies the original array, not necessarily a deep copy
+#grades.resize(1, 6)
+
+# Method flatten deep copies the original array's data:
+# always one dimensional array
+flattened = grades.flatten()
+# alternatively, Methods ravel produces a view (shallow copy) of the original array
+# which shares the grades array's data:
+
+raveled = grades.ravel()
+
+raveled[0] = 100
+
+raveled[5] = 99
+
+
+print()  # run and debug values seen in debug
